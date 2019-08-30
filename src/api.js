a62b939e587cc6b4a90f89c0ef6eb57c20006912
newstar <kangsaebyeol@gmail.com>
2019-08-31 00:51:09 +0900

    API Verbs part One

--- src/api.js
+++ src/api.js
@@ -8,6 +8,16 @@ const api = axios.create({
     }
 });
 
-api.get("tv/popular"); // tv 앞에 '/'를 넣으면 절대경로가 되기 때문에 넣으면 안 됨
+; // tv 앞에 '/'를 넣으면 절대경로가 되기 때문에 넣으면 안 됨
 
-export default api;
+export const tvApi = {
+    nowPlaying: () => api.get('movie/now_playing'),
+    upcoming: () => api.get('movie/upcoming'),
+    popular: () => api.get("movie/popular")
+}
+
+export const moviesApi = {
+    topRated: () => api.get('tv/top_rated'),
+    popular: () => api.get("tv/popular"),
+    airingToday: () => api.get("tv/airing_today")
+}
