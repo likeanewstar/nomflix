e5f5ad0e07300a76b7cea3a28234c630e5e64eeb
newstar <kangsaebyeol@gmail.com>
2019-08-31 18:17:17 +0900

    Modify search container

--- src/Routes/Search/SearchContainer.js
+++ src/Routes/Search/SearchContainer.js
@@ -1,25 +1,52 @@
 import React from "react";
 import SearchPresenter from "./SearchPresenter";
-import { moviesApi } from "api";
-import { tvApi } from "api";
+import { moviesApi, tvApi } from "api";
 
 export default class extends React.Component {
     state = {
         movieResults: null,
         tvResults: null,
-        searchTerm: "",
+        searchTerm: "code",
         error: null,
         loading: false
     };
 
+    handleSubmit = () => {
+        const { searchTerm } = this.state;
+        if (searchTerm !== "") {
+          this.searchByTerm();
+        }
+    };
+
+    searchByTerm = async() => {
+        const { searchTerm } = this.state;
+        this.setState({loading: true})
+        try {
+            const {data: {results: movieResults}} = await moviesApi.search(searchTerm);
+            const {data: {results: tvResults}} = await tvApi.search(searchTerm);
+            this.setState({
+                movieResults,
+                tvResults
+            });
+        } catch {
+            this.setState({
+                error: "Can't find results."
+            })
+        } finally {
+            this.setState({loading: false})
+        }
+    }
+
     render() {
         const { movieResults, tvResults, searchTerm, error, loading } = this.state;
+        console.log(this.state)
         return <SearchPresenter 
             movieResults={movieResults}       
             tvResults={tvResults}     
             searchTerm={searchTerm}     
             error={error}     
             loading={loading}     
+            handleSubmit={this.handleSubmit}
         />
     }
 }
